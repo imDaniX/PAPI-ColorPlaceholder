2ccd5795ced8c9cb0ebaeb1ec3685d16cc1213ba
Daniil Z <idanix@list.ru>
2023-05-08 16:16:05 +0300

    Post the actual stuff Thanks IDEA, very cool

--- src/main/java/me/imdanix/papi/color/ColorExpansion.java
+++ src/main/java/me/imdanix/papi/color/ColorExpansion.java
@@ -1,21 +1,51 @@
 package me.imdanix.papi.color;
 
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import me.clip.placeholderapi.expansion.Relational;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-public class ColorExtension extends PlaceholderExpansion {
+import static me.clip.placeholderapi.PlaceholderAPI.setBracketPlaceholders;
+import static me.clip.placeholderapi.PlaceholderAPI.setRelationalPlaceholders;
+import static org.bukkit.ChatColor.translateAlternateColorCodes;
+
+public class ColorExpansion extends PlaceholderExpansion implements Relational {
     @Override
     public @NotNull String getIdentifier() {
-        return null;
+        return "color";
     }
 
     @Override
     public @NotNull String getAuthor() {
-        return null;
+        return "imDaniX";
     }
 
     @Override
     public @NotNull String getVersion() {
-        return null;
+        return "1.0";
+    }
+
+    @Override
+    public String onRequest(OfflinePlayer player, @NotNull String params) {
+        String bracket = "{" + params + "}";
+        String parsed = setBracketPlaceholders(player, bracket);
+        return bracket.equals(parsed)
+                ? null
+                : translateAlternateColorCodes('&', parsed);
+    }
+
+    @Override
+    public String onPlaceholderRequest(Player player, @NotNull String params) {
+        return onRequest(player, params);
+    }
+
+    @Override
+    public String onPlaceholderRequest(Player player1, Player player2, String params) {
+        String bracket = "{" + params + "}";
+        String parsed = setRelationalPlaceholders(player1, player2, bracket);
+        return bracket.equals(parsed)
+                ? null
+                : translateAlternateColorCodes('&', parsed);
     }
 }
--- src/test/java/me/imdanix/papi/color/ColorExpansionTest.java
+++ src/test/java/me/imdanix/papi/color/ColorExpansionTest.java
@@ -1,9 +1,48 @@
 package me.imdanix.papi.color;
 
-import static org.testng.Assert.*;
-public class ColorExpansionTest {
+import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
-    @org.testng.annotations.Test
-    public void testOnRequest() {
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.nullable;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertEquals;
+
+public class ColorExpansionTest {
+    private final ColorExpansion expansion = new ColorExpansion();
+    private final Player mockPlayer = Mockito.mock(Player.class);
+
+    @BeforeClass
+    public void setup() {
+        when(mockPlayer.getUniqueId()).thenReturn(UUID.randomUUID());
+    }
+
+    @DataProvider
+    public Object[][] onRequestData() {
+        return new Object[][] {
+                {"placeholder", "&aHello", "§aHello"},
+                {"placeholder", "&aHello&x&1&2&3&4&5&6", "§aHello§x§1§2§3§4§5§6"},
+                {"placeholder", "Test", "Test"},
+                {"placeholder", "{placeholder}", null}
+        };
+    }
+
+    @Test(dataProvider = "onRequestData")
+    public void onRequestTest(String params, String phResult, String expected) {
+        try (MockedStatic<PlaceholderAPI> mockPapi = Mockito.mockStatic(PlaceholderAPI.class)) {
+            mockPapi.when(() -> PlaceholderAPI.setBracketPlaceholders(nullable(OfflinePlayer.class), nullable(String.class)))
+                    .thenReturn(phResult);
+            assertEquals(
+                    expansion.onPlaceholderRequest(mockPlayer, params),
+                    expected
+            );
+        }
     }
 }
