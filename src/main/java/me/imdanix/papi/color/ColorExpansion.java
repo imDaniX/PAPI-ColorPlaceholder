80bf08a69ce2d7f8aebfd8b2ed538d1737f547c2
Daniil Z <idanix@list.ru>
2023-05-08 16:15:08 +0300

    Initial commit

---
+++ src/main/java/me/imdanix/papi/color/ColorExpansion.java
@@ -0,0 +1,21 @@
+package me.imdanix.papi.color;
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.jetbrains.annotations.NotNull;
+
+public class ColorExtension extends PlaceholderExpansion {
+    @Override
+    public @NotNull String getIdentifier() {
+        return null;
+    }
+
+    @Override
+    public @NotNull String getAuthor() {
+        return null;
+    }
+
+    @Override
+    public @NotNull String getVersion() {
+        return null;
+    }
+}
---
+++ src/test/java/me/imdanix/papi/color/ColorExpansionTest.java
@@ -0,0 +1,9 @@
+package me.imdanix.papi.color;
+
+import static org.testng.Assert.*;
+public class ColorExpansionTest {
+
+    @org.testng.annotations.Test
+    public void testOnRequest() {
+    }
+}
